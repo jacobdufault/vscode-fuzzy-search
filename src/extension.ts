d26ae33e136c35d409cca65bcb685550bcbccf64
Jacob Dufault <jacobdufault@gmail.com>
2018-08-23 11:53:02 -0700

    Previous search and selection is now restored on next invocation
    - Made possible with the new quick pick API.
    - Also fixed off-by-one line number

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import * as vscode from 'vscode';
 
 class Item implements vscode.QuickPickItem {
@@ -15,59 +14,93 @@ function pad(str: string, length: number) {
   return '0'.repeat(length - str.length) + str
 }
 
+let valueFromPreviousInvocation = '';
+let lastSelected: Item = undefined;
+
 function showQuickPick() {
-  // Build the entries we will show the user. One entry for each non-empty
-  // line, prefixed with the line number. We prefix with the line number so
-  // lines stay in the correct order and so duplicate lines do not get merged
-  // together.
+  // Build the entries we will show the user. One entry for each non-empty line,
+  // prefixed with the line number. We prefix with the line number so lines stay
+  // in the correct order and so duplicate lines do not get merged together.
   let lines: string[] =
-      vscode.window.activeTextEditor.document.getText().split(/\r?\n/);
+    vscode.window.activeTextEditor.document.getText().split(/\r?\n/);
   let maxNumberLength = lines.length.toString().length;
-  let quickPickEntries = [];
+  let quickPickEntries: Item[] = [];
   for (let i = 0; i < lines.length; ++i) {
     if (lines[i]) {
       quickPickEntries.push(
-          new Item(`${pad(i.toString(), maxNumberLength)}: ${lines[i]}`, i));
+        new Item(`${pad((i + 1).toString(), maxNumberLength)}: ${lines[i]}`, i));
     }
   }
 
-  // vscode calls this on the first item. We don't want to navigate to there
-  // right away.
-  let ignoreFirstNavigate = true;
-  // If the user cancels the navigation we want to go back to the original
-  // location in the document.
-  let startingSelection = vscode.window.activeTextEditor.selection;
-  function navigateTo(item: Item|undefined) {
-    if (ignoreFirstNavigate) {
-      ignoreFirstNavigate = false;
-      return;
-    }
+  // Setup basic quick pick.
+  let pick = vscode.window.createQuickPick<Item>();
+  pick.items = quickPickEntries;
+  pick.canSelectMany = false;
 
-    // Failed.
-    if (!item) {
-      vscode.window.activeTextEditor.revealRange(
-          new vscode.Range(startingSelection.start, startingSelection.end),
-          vscode.TextEditorRevealType.InCenter);
-      vscode.window.activeTextEditor.selection = startingSelection;
-      return;
-    }
 
-    // Selected.
-    let p = new vscode.Position(item.line, 0);
+  // Try to preselect the previously selected item.
+  if (lastSelected) {
+    // Update `lastSelected` reference to point to the current entry in `items`.
+    lastSelected = quickPickEntries.find(
+      t => t.line == lastSelected.line || t.label == lastSelected.label);
+  }
+  pick.activeItems = [lastSelected];
+  // Save the item the user selected so it can be pre-selected next time fuzzy
+  // search is invoked.
+  pick.onDidAccept(() => {
+    lastSelected = pick.selectedItems[0];
+    pick.hide();
+  });
+
+
+  // Show the currently selected item in the editor.
+  pick.onDidChangeActive(items => {
+    let p = new vscode.Position(items[0].line, 0);
     vscode.window.activeTextEditor.revealRange(
-        new vscode.Range(p, p), vscode.TextEditorRevealType.InCenter);
+      new vscode.Range(p, p), vscode.TextEditorRevealType.InCenter);
     vscode.window.activeTextEditor.selection = new vscode.Selection(p, p);
-  };
+  });
 
-  // Without the |then| we do not get cancellation calls.
-  vscode.window
-      .showQuickPick(
-          quickPickEntries,
-          <vscode.QuickPickOptions>{onDidSelectItem: navigateTo})
-      .then(navigateTo);
+
+  // Show the previous search string. When the user types a character, the
+  // preview string will replaced with the typed character.
+  pick.value = valueFromPreviousInvocation;
+  let previewValue = valueFromPreviousInvocation;
+  let hasPreviewValue = previewValue.length > 0;
+  pick.onDidChangeValue(value => {
+    if (hasPreviewValue) {
+      hasPreviewValue = false;
+
+      // Try to figure out what text the user typed. Assumes that the user typed
+      // at most one character.
+      for (let i = 0; i < value.length; ++i) {
+        if (previewValue.charAt(i) != value.charAt(i)) {
+          pick.value = value.charAt(i);
+          break;
+        }
+      }
+    }
+  });
+  // Save the search string so we can show it next time fuzzy search is invoked.
+  pick.onDidChangeValue(value => valueFromPreviousInvocation = value);
+
+
+  // If fuzzy-search was cancelled navigate to the previous location.
+  let startingSelection = vscode.window.activeTextEditor.selection;
+  pick.onDidHide(() => {
+    if (pick.selectedItems.length == 0) {
+      vscode.window.activeTextEditor.revealRange(
+        new vscode.Range(startingSelection.start, startingSelection.end),
+        vscode.TextEditorRevealType.InCenter);
+      vscode.window.activeTextEditor.selection = startingSelection;
+    }
+  });
+
+
+  pick.show();
 }
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
   vscode.commands.registerCommand(
-      'fuzzySearch.activeTextEditor', showQuickPick);
+    'fuzzySearch.activeTextEditor', showQuickPick);
 }
