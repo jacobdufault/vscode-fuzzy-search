3473b97cc814abecdc95ab3b230c0a953bb33f20
Muah <ctf0@users.noreply.github.com>
2019-05-16 23:17:23 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -55,12 +55,12 @@ function showFuzzySearch(useCurrentSelection: boolean) {
 
   // Show the currently selected item in the editor.
   pick.onDidChangeActive(items => {
-    if (items.length) {
-      let p = new vscode.Position(items[0].line, 0);
-      vscode.window.activeTextEditor.revealRange(
-      new vscode.Range(p, p), vscode.TextEditorRevealType.InCenter);
-      vscode.window.activeTextEditor.selection = new vscode.Selection(p, p);
-    }
+    if (!items.length)  return;
+    
+    let p = new vscode.Position(items[0].line, 0);
+    vscode.window.activeTextEditor.revealRange(
+    new vscode.Range(p, p), vscode.TextEditorRevealType.InCenter);
+    vscode.window.activeTextEditor.selection = new vscode.Selection(p, p);
   });
 
 
